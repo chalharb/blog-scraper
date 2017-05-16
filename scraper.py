33a841f16f4740f9107cfe366f957b97a1191640
fsa7397 <logan.harber@hcahealthcare.com>
2017-05-16 11:22:59 -0500

    refactored code and added directory creation

--- scraper.py
+++ scraper.py
@@ -4,17 +4,15 @@
 # Logan Harber
 ##################
 
+import os
 import requests
 from bs4 import BeautifulSoup
-
-url = "https://medicalcitychildrenshospitalurgentcareblog.wordpress.com/page/"
-pageCount = 10
-full_html = []
 
 # Pings url duh.
 def pingPage(url):
     # Requires http or https
     response = requests.get(url)
+    response.raise_for_status()
 
     data = response.text
 
@@ -22,21 +20,43 @@ def pingPage(url):
 
 
 def build_url(url, pageCount):
-    pageCountToString = str(pageCount)
-    new_url = url + pageCountToString
+    new_url = url + str(pageCount)
     return new_url
 
 
-for i in range(pageCount):
-    i += 1
-
-    builtURL = build_url(url, i)  # new url for each page
-    print builtURL
-    data = pingPage(builtURL)
-
-    soup = BeautifulSoup(data, 'html.parser')
-    print(soup.find_all("article"))
+def createDirectory(output_dir):
+    desired_permission = 0o777
+    try:
+        original_umask = os.umask(0)
+        os.makedirs(output_dir, desired_permission)
+    finally:
+        os.umask(original_umask)
 
 
-print full_html
-print len(full_html)
+def main():
+    url = "https://medicalcitychildrenshospitalurgentcareblog.wordpress.com/page/"
+    page_start = 1
+    output_dir = "output-files"
+
+    # check if directory exists
+    if os.path.isdir(output_dir):
+        print("Output directory already exists!")
+    else:
+        print("Creating Output Directory...")
+        createDirectory(output_dir)
+
+    while True:
+        try:
+            built_url = build_url(url, page_start)  # new url for each page
+            print(built_url)
+            data = pingPage(built_url)
+
+            soup = BeautifulSoup(data, 'html.parser')
+            print(soup.find_all("article"))
+        except Exception as e:
+            print("No page found. We're probably done. Aborting.")
+            break
+
+        page_start += 1
+
+if  __name__ =='__main__':main()
